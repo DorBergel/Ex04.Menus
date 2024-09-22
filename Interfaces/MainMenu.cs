b699d4238c260d8aa835117528490066fe3ab587
Dor Bergel <dor.bergel9@gmail.com>
2024-09-22 19:55:00 +0300

    Add project files.

---
+++ Interfaces/MainMenu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class MainMenu
+    {
+        MenuItem m_MainMenu;
+
+
+        public MainMenu(MenuItem i_MenuItem)
+        {
+            m_MainMenu = i_MenuItem;
+        }
+
+        public void Show()
+        {
+            int choice;
+
+            while (true)
+            {
+                DisplayMenu(m_MainMenu);
+
+                choice = GetValidInput(m_MainMenu.SubMenus.Count);
+                var selectedItem = m_MainMenu.SubMenus[choice - 1];
+
+                if (selectedItem.MenuHasSubItem())
+                {
+                    m_MainMenu = selectedItem;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    selectedItem.Action?.Invoke();
+                    Console.WriteLine("Press any key to return to the menu...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+
+        private void DisplayMenu(MenuItem i_MenuItem)
+        {
+            Console.WriteLine($"** {i_MenuItem.Title} **");
+            Console.WriteLine($"--------------------");
+
+            for (int i = 0; i < i_MenuItem.SubMenus.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {i_MenuItem.SubMenus[i].Title}");
+            }
+        }
+
+        private int GetValidInput(int i_MaxOption)
+        {
+            int userChoice;
+
+            while(true)
+            {
+                Console.Write($"Please enter your choice ({1}-{i_MaxOption - 1} or 0 to {'*'}"); // TODO think of the condition to when it should display 'back' or 'exit'
+                string userInput = Console.ReadLine();
+                
+                if(int.TryParse(userInput, out userChoice) && userChoice >= 0 && userChoice <= i_MaxOption)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid input, please try again.");
+            }
+
+            return userChoice;
+        }
+    }
+}
---
+++ Interfaces/MenuItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class MenuItem
+    {
+        string m_Title;
+        List<MenuItem> m_SubMenus;
+        Action m_Action;
+
+        public string Title
+        {
+            get
+            {
+                return m_Title;
+            }
+        }
+
+        public List<MenuItem> SubMenus
+        {
+            get
+            {
+                return m_SubMenus;
+            }
+        }
+
+        public Action Action
+        {
+            get
+            {
+                return Action;
+            }
+        }
+
+        public MenuItem(string i_Title, Action i_Action=null)
+        {
+            m_Title = i_Title;
+            m_SubMenus = new List<MenuItem>();
+            m_Action = i_Action;
+        }
+
+        public void AddMenu(MenuItem i_MenuItem)
+        {
+            m_SubMenus.Add(i_MenuItem);
+        }
+
+        public bool MenuHasSubItem()
+        {
+            return m_SubMenus.Count > 0;
+        }
+    }
+}
